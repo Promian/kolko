89058d49cfa49ac151fde520566d9c44dba51133
Promian <marcin.promianowski@gmail.com>
2021-12-18 15:26:44 +0100

    zwierzekoniec

---
+++ klasy/pojazdy.py
@@ -0,0 +1,13 @@
+class Pojazd:
+    def __init__(self, nazwa, wiek, waga)
+    self.nazwa = nazwa
+    self.wiek = wiek
+    self.waga = wags
+
+class Samochod(Pojazd):
+    def __init__(self):
+        self.ilu_osobowe = 5
+
+class Motocykl(Pojazd):
+    def __init__(self):
+        self.rodzaj = 
--- klasy/zwierze.py
+++ klasy/zwierze.py
@@ -14,6 +14,24 @@ class Lew(Zwierze):
     def __init__(self):
         self.ilosc_klow = 4
 
+class Papuga(Zwierze):
+    def __init__(self, nazwa,dlg_skrzydel):
+        self.ilosc_pior = 800
+        self.nazwa = nazwa
+        self.dlg_skrzydel = dlg_skrzydel
+
+class Hybryda(Lew, Papuga):
+    pass
+
+hyb = Hybryda()
+hyb.ilosc_klow = 4
+hyb.nazwa = "HUHUHS"
+hyb.waga = 100
+hyb.ilosc_pior = 300
+hyb.wiek = 34
+
+hyb.podaj_dane()
+
 Dumbo = Slon("Slonik Dumbo", 300, 4)
 
 Simba = Lew()
