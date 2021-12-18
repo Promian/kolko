6baea9ae550109b21f8919536a7f17bf5623b352
Promian <marcin.promianowski@gmail.com>
2021-12-18 10:30:18 +0100

    skończona plansza do gry

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #tu będzie gra kółko i krzyżyk
+
+plansza_do_gry = {'7': ' ', '8': ' ', '9': ' ', '4': ' ', '5': ' ', '6': ' ', '1': ' ', '2': ' ', '3': ' '}
+
+klawisze_gry = []
+
+for key in plansza_do_gry:
+    klawisze_gry.append(key)
+    print(klawisze_gry)
+
+def drukuj_plansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print("-+-+-")
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print("-+-+-")
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
+drukuj_plansze(plansza_do_gry)
+
